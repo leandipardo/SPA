29edbba82d84111136cac784388db30d548dfd00
DipploX <martindipardo@hotmail.com>
2023-08-21 20:02:16 -0300

    sincro

---
+++ js/juego.js
@@ -0,0 +1,16 @@
+// const d = document;
+// const section = d.getElementById("seccion2");
+
+// //crear maquetacion
+
+// const div = d.createElement("div");
+// div.className = "tablero";
+
+// const pacman = d.createElement("div");
+// pacman.className = "pacman";
+
+
+
+// d.addEventListener("onkeydown", e=> ){
+//     if(e.addEventListener("key")
+// }
--- js/modulos.js
+++ js/modulos.js
@@ -1,7 +1,9 @@
 import menuSlide from "./menu.js";
 import alarm from "./reloj.js";
+// import juego from "./juego.js";
 const d = document;
 d.addEventListener("DOMContentLoaded", (e)=>{
     menuSlide(".button-menu",".scuare",".menu");
     alarm(".clock", ".clock-off", ".sound",".sound-off");
+    // juego("");
 })
