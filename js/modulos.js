b7a6a1afdd039207c97080bcf06c5235dbffb4f1
leandipardo <leandipardo@gmail.com>
2023-08-21 21:42:00 -0300

    21231231

--- js/modulos.js
+++ js/modulos.js
@@ -14,34 +14,27 @@ d.addEventListener("DOMContentLoaded", (e)=>{
   let x=50;
   let y=50;
   function disableArrowScroll(event){
+    event.preventDefault();
     const $pacman = d.querySelector(".pacman");
-    if (event.key === "ArrowUp") {
+    if (event.key === "ArrowUp" && y >= 0) {
         console.log("entro");
         y--;
-        event.preventDefault();
-        $pacman.setAttribute(`style`,`top:${y}%;left:${y}%`);
-        console.log(`el valor de y es : ${y}`);
+        $pacman.setAttribute(`style`,`top:calc(${y}% - 30px);left:calc(${x}% - 30px)`);
     }
-    if (event.key === "ArrowDown") {
+    if (event.key === "ArrowDown" && y <= 100) {
         console.log("entro");
         y++;
-        event.preventDefault();
-        $pacman.setAttribute(`style`,`top:${y}%;left:${y}%`);
-        console.log(`el valor de y es : ${y}`);
+        $pacman.setAttribute(`style`,`top:calc(${y}% - 30px);left:calc(${x}% - 30px)`);
     }
-    if (event.key === "ArrowLeft") {
+    if (event.key === "ArrowLeft" && x >= 0) {
         x--;
         console.log("entro");
-        event.preventDefault();
-        $pacman.setAttribute(`style`,`top:${y}%;left:${y}%`);
-        console.log(`el valor de x es : ${x}`);
+        $pacman.setAttribute(`style`,`top:calc(${y}% - 30px);left:calc(${x}% - 30px)`);
     }
-    if (event.key === "ArrowRight") {
+    if (event.key === "ArrowRight" && x <= 100) {
         x++;
         console.log("entro");
-        event.preventDefault();
-        $pacman.setAttribute(`style`,`top:${y}%;left:${y}%`);
-        console.log(`el valor de x es : ${x}`);
+        $pacman.setAttribute(`style`,`top:calc(${y}% - 30px);left:calc(${x}% - 30px)`);
     }
 
 
