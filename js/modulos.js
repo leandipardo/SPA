606aa539a8bfb9e73d3e3c90e9aefa319469eda2
DipploX <martindipardo@hotmail.com>
2023-08-24 09:13:16 -0300

    corregido

--- js/modulos.js
+++ js/modulos.js
@@ -1,10 +1,12 @@
 import menuSlide from "./menu.js";
 import alarm from "./reloj.js";
 import {movepacman, shortcuts,badisimo} from "./juego.js";
+import countdown from "./countdown.js";
 const d = document;
 d.addEventListener("DOMContentLoaded", (e)=>{
     menuSlide(".button-menu",".scuare",".menu");
     alarm(".clock", ".clock-off", ".sound",".sound-off");
+    countdown();
 })
 badisimo();
 d.addEventListener("keydown", (e)=>{
